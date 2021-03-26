f95a6c0242dca0d7ded5fd161c74ff210ab15736
KOSEKI Yoshinori <koseki.y@gmail.com>
2021-03-26 16:29:33 +0900

    initial commit

---
+++ macOS/opencv_with-contrib.sh
@@ -0,0 +1,17 @@
+# OpenCV: macOS Apple silicon, Intel
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2021.3/opencv \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_INF_ENGINE=ON \
+    -DENABLE_CXX11=ON \
+    -DWITH_TBB=ON \
+    -DPYTHON3_EXECUTABLE=$HOME/miniforge3/bin/python$PYVAR \
+    -DPYTHON3_LIBRARY=$HOME/miniforge3/lib/libpython$PYVAR.dylib \
+    -DPYTHON3_INCLUDE_DIR=$HOME/miniforge3/include/python$PYVAR \
+    -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    ..
+
+# end
+
+
