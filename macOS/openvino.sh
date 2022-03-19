7dc1e768842291f6cd1ddd5c4e995c83eee63c89
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-03-19 21:31:35 +0900

    auto select python_version

--- macOS/openvino.sh
+++ macOS/openvino.sh
@@ -1,7 +1,10 @@
 # OpenVINO: maxOS Apple silicon, Intel
 
-if test "$PYVAR" = ""; then
-    export PYVAR=3.9
+python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
+
+if test ! -x $HOME/miniforge3/bin/python$python_version; then
+    echo "No exist: $HOME/miniforge3/bin/python$python_version"
+    exit 0
 fi
 
 if test -d $HOME/miniforge3; then
@@ -12,9 +15,9 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2021.4.2 \
     -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_PYTHON=ON \
-    -DPYTHON_EXECUTABLE=$HOME/miniforge3/bin/python$PYVAR \
-    -DPYTHON_LIBRARY=$HOME/miniforge3/lib/libpython$PYVAR.dylib \
-    -DPYTHON_INCLUDE_DIR=$HOME/miniforge3/include/python$PYVAR \
+    -DPYTHON_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
+    -DPYTHON_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
+    -DPYTHON_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
     ..
 
 
