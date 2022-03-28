c3f7bc9329797ce16d4a1c44e3423b63ff537b32
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-03-28 10:49:13 +0900

    add new.

---
+++ macOS/openvino_with-arm-plugin.sh
@@ -0,0 +1,31 @@
+# OpenVINO: maxOS Apple silicon, Intel
+
+python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
+
+if test ! -x $HOME/miniforge3/bin/python$python_version; then
+    echo "No exist: $HOME/miniforge3/bin/python$python_version"
+    exit 0
+fi
+
+if test -d $HOME/miniforge3; then
+    conda install -y numpy matplotlib cython pandas scipy scikit-learn pyyaml
+fi
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_PYTHON=ON \
+    -DPYTHON_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
+    -DPYTHON_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
+    -DPYTHON_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
+    -DIE_EXTRA_MODULES=../../openvino_contrib/modules \
+    -DBUILD_java_api=OFF \
+    ..
+
+
+# if test ! -f ../scripts/setupvars/setupvars.sh.orig; then
+#     cd ../scripts/setupvars
+#     patch --backup --verbose < ../../../setupvars.sh.patch
+# fi
+
+# end
