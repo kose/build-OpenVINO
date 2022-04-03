adc1ab97da4424aa541356f29b82e7ed8938cb4c
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-04-03 11:49:48 +0900

    for 2022.1

--- Ubuntu/opencv_with-contrib.sh
+++ Ubuntu/opencv_with-contrib.sh
@@ -1,18 +1,16 @@
 # Ubuntu Raspberry Pi4
 
-export PYVAR=3.8
-
-source ../../dot.rc
+python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 
 cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2021.4.2/opencv \
+    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/opencv \
     -DCMAKE_BUILD_TYPE=Release \
     -DWITH_INF_ENGINE=ON \
     -DENABLE_CXX11=ON \
     -DWITH_TBB=ON \
-    -DPYTHON_EXECUTABLE=/usr/bin/python$PYVAR \
-    -DPYTHON_LIBRARY=/usr/lib/aarch64-linux-gnu/libpython$PYVAR.so \
-    -DPYTHON_INCLUDE_DIR=/usr/include/python$PYVAR \
+    -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
+    -DPYTHON_LIBRARY=/usr/lib/aarch64-linux-gnu/libpython$python_version.so \
+    -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
     -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
     ..
 
--- Ubuntu/openvino_with-arm-plugin.sh
+++ Ubuntu/openvino_with-arm-plugin.sh
@@ -1,28 +1,25 @@
 # OpenVINO: Ubuntu Raspberry Pi4
 
-export PYVAR=3.8
-
-source ../../dot.rc
+python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 
 cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2021.4.2 \
+    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
     -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_SSE42=OFF \
     -DTHREADING=SEQ \
     -DENABLE_GNA=OFF \
     -DENABLE_PYTHON=ON \
-    -DPYTHON_EXECUTABLE=/usr/bin/python$PYVAR \
-    -DPYTHON_LIBRARY=/usr/lib/aarch64-linux-gnu/libpython$PYVAR.so \
-    -DPYTHON_INCLUDE_DIR=/usr/include/python$PYVAR \
+    -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
+    -DPYTHON_LIBRARY=/usr/lib/aarch64-linux-gnu/libpython$python_version.so \
+    -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
     -DIE_EXTRA_MODULES=../../openvino_contrib/modules \
     -DBUILD_java_api=OFF \
     -DENABLE_MYRIAD=OFF \
     ..
 
-
 if test ! -f ../scripts/setupvars/setupvars.sh.orig; then
-    cd ../scripts/setupvars
-    patch --backup --verbose < ../../../setupvars.sh.patch
+    cd ..
+    patch --backup --verbose -p1 < ../setupvars.sh.patch
 fi
 
 # end
