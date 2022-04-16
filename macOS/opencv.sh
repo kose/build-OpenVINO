e80166bc759da9f9324073852b3f914f7c89c6d3
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-04-16 13:02:15 +0900

    add make & tee

--- macOS/opencv.sh
+++ macOS/opencv.sh
@@ -2,11 +2,13 @@
 
 python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 
+#
 if test ! -x $HOME/miniforge3/bin/python$python_version; then
     echo "No exist: $HOME/miniforge3/bin/python$python_version"
     exit 0
 fi
 
+#
 cmake \
     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/extras/opencv \
     -DCMAKE_BUILD_TYPE=Release \
@@ -17,6 +19,7 @@ cmake \
     -DPYTHON3_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
     -DPYTHON3_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
     -DPYTHON3_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
-    ..
+    .. \
+    && make -j 6 2>&1 | tee /tmp/`basename $0`.log
 
 # end
