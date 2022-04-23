bd8a8a15f4367d589419b537edef8ebd10aaa59e
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-04-23 10:23:16 +0900

    Fix loged cmakeing

--- macOS/opencv.sh
+++ macOS/opencv.sh
@@ -9,19 +9,19 @@ if test ! -x $HOME/miniforge3/bin/python$python_version; then
 fi
 
 #
-cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/extras/opencv \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DWITH_INF_ENGINE=ON \
-    -DENABLE_CXX11=ON \
-    -DBUILD_TBB=ON \
-    -DBUILD_opencv_gapi=ON \
-    -DBUILD_ZLIB=OFF \
-    -DPYTHON3_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
-    -DPYTHON3_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
-    -DPYTHON3_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
-    .. \
-    && make -j 6 2>&1 | tee /tmp/`basename $0`.log
+(cmake \
+     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/extras/opencv \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DWITH_INF_ENGINE=ON \
+     -DENABLE_CXX11=ON \
+     -DBUILD_TBB=ON \
+     -DBUILD_opencv_gapi=ON \
+     -DBUILD_ZLIB=OFF \
+     -DPYTHON3_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
+     -DPYTHON3_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
+     -DPYTHON3_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
+     .. \
+     && make -j 6) 2>&1 | tee /tmp/`basename $0`.log
 
 exit 0
 # end
--- macOS/openvino_with-arm-plugin.sh
+++ macOS/openvino_with-arm-plugin.sh
@@ -11,27 +11,27 @@ fi
 conda install -y numpy matplotlib cython pandas scipy scikit-learn pyyaml pybind11
 
 #
+(cmake \
+     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DENABLE_SSE42=OFF \
+     -DTHREADING=SEQ \
+     -DENABLE_GNA=OFF \
+     -DENABLE_MKL_DNN=ON \
+     -DARM_COMPUTE_SCONS_JOBS=4 \
+     -DENABLE_PYTHON=ON \
+     -DPYTHON_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
+     -DPYTHON_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
+     -DPYTHON_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
+     -DIE_EXTRA_MODULES=`pwd`/../../openvino_contrib/modules/arm_plugin \
+     .. \
+     && make -j 6) 2>&1 | tee /tmp/`basename $0`.log
+
+#
 if test ! -f ../scripts/setupvars/setupvars.sh.orig; then
     cd ..
     patch --backup --verbose -p1 < ../setupvars.sh.patch || exit -1
 fi
-
-#
-cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DENABLE_SSE42=OFF \
-    -DTHREADING=SEQ \
-    -DENABLE_GNA=OFF \
-    -DENABLE_MKL_DNN=ON \
-    -DARM_COMPUTE_SCONS_JOBS=4 \
-    -DENABLE_PYTHON=ON \
-    -DPYTHON_EXECUTABLE=$HOME/miniforge3/bin/python$python_version \
-    -DPYTHON_LIBRARY=$HOME/miniforge3/lib/libpython$python_version.dylib \
-    -DPYTHON_INCLUDE_DIR=$HOME/miniforge3/include/python$python_version \
-    -DIE_EXTRA_MODULES=`pwd`/../../openvino_contrib/modules/arm_plugin \
-    .. \
-    && make -j 6 2>&1 | tee /tmp/`basename $0`.log
 
 exit 0
 # end
