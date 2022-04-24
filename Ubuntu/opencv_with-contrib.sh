bea93a0e3f51107f687b595a6d85f7a21ba3442d
KOSEKI Yoshinori <koseki.y@gmail.com>
2022-04-24 09:08:05 +0900

    add logging

--- Ubuntu/opencv_with-contrib.sh
+++ Ubuntu/opencv_with-contrib.sh
@@ -2,17 +2,18 @@
 
 python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/extras/opencv \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DWITH_INF_ENGINE=ON \
-    -DENABLE_CXX11=ON \
-    -DWITH_TBB=ON \
-    -DWITH_1394=OFF \
-    -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
-    -DPYTHON_LIBRARY=/usr/lib/`arch`-linux-gnu/libpython$python_version.so \
-    -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
-    -DOPENCV_EXTRA_MODULES_PATH=`pwd`/../../opencv_contrib/modules \
-    ..
+(cmake \
+     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1/extras/opencv \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DWITH_INF_ENGINE=ON \
+     -DENABLE_CXX11=ON \
+     -DWITH_TBB=ON \
+     -DWITH_1394=OFF \
+     -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
+     -DPYTHON_LIBRARY=/usr/lib/`arch`-linux-gnu/libpython$python_version.so \
+     -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
+     -DOPENCV_EXTRA_MODULES_PATH=`pwd`/../../opencv_contrib/modules \
+     .. \
+     && make -j 6) 2>&1 | tee /tmp/`basename $0`.log
 
 # end
--- Ubuntu/openvino.sh
+++ Ubuntu/openvino.sh
@@ -2,16 +2,17 @@
 
 python_version=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DENABLE_SSE42=OFF \
-    -DTHREADING=SEQ \
-    -DENABLE_PYTHON=ON \
-    -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
-    -DPYTHON_LIBRARY=/usr/lib/`arch`-linux-gnu/libpython$python_version.so \
-    -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
-    ..
+(cmake \
+     -DCMAKE_INSTALL_PREFIX=/opt/intel/openvino_2022.1 \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DENABLE_SSE42=OFF \
+     -DTHREADING=SEQ \
+     -DENABLE_PYTHON=ON \
+     -DPYTHON_EXECUTABLE=/usr/bin/python$python_version \
+     -DPYTHON_LIBRARY=/usr/lib/`arch`-linux-gnu/libpython$python_version.so \
+     -DPYTHON_INCLUDE_DIR=/usr/include/python$python_version \
+     .. \
+     && make -j 6) 2>&1 | tee /tmp/`basename $0`.log
 
 if test ! -f ../scripts/setupvars/setupvars.sh.orig; then
     cd ..
